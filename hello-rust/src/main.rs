b4c8b9d6f0771713ba9d6ffd60e106ab943ecbfd
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2024-05-14 20:27:52 +0530

    add file read

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,8 +1,12 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::env::current_dir;
+use std::fs::File;
+use std::io::stdout;
+use std::io::BufWriter;
+use std::io::Read;
+use std::str::from_utf8;
 
-
-fn main() {
+fn hello() {
     println!("Hello, world!");
     let stdout = stdout();
     let message = String::from("Hello from Santhosh");
@@ -11,4 +15,37 @@ fn main() {
 
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
+}
+
+fn read_file() {
+    let current_dir = current_dir().unwrap().display().to_string() + "/src";
+    let path = current_dir + "/hello.txt";
+    let mut file = match File::open(path) {
+        Err(error) => panic!("Error opening the file: {}", error),
+        Ok(value) => value,
+    };
+
+    let stat = match file.metadata() {
+        Err(error) => panic!("Error getting metadata: {}", error),
+        Ok(value) => value,
+    };
+
+    let mut buffer = vec![0; stat.len() as usize];
+
+    match file.read(&mut buffer) {
+        Err(error) => panic!("Error reading the file: {}", error),
+        Ok(_) => (),
+    };
+
+    let data = match from_utf8(&buffer) {
+        Err(error) => panic!("Error converting to string: {}", error),
+        Ok(value) => value,
+    };
+
+    println!("Content is: {}", data);
+}
+
+fn main() {
+    hello();
+    read_file();
 }
