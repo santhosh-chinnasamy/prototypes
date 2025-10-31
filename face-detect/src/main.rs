aa9b444bdeeb9a984d69e666ce55caa2bb36ce00
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2025-10-31 19:38:40 +0530

    run inference with ML model and get shapes

--- face-detect/src/main.rs
+++ face-detect/src/main.rs
@@ -1,14 +1,15 @@
 use anyhow::{Error, Ok};
 use image::{
-    DynamicImage,
+    DynamicImage, GenericImageView,
     imageops::{self, FilterType},
 };
-use std::{time::Instant};
+use std::time::Instant;
+use tract_ndarray::{Array2, Array4, Axis, Ix2};
 use tract_onnx::prelude::*;
 
 fn main() -> Result<(), Error> {
     let t = Instant::now();
-    let _model = tract_onnx::onnx()
+    let model = tract_onnx::onnx()
         .model_for_path("models/yolov8n-face.onnx")?
         .with_input_fact(0, f32::fact([1, 3, 640, 640]).into())?
         .into_optimized()?
@@ -32,15 +33,36 @@ fn main() -> Result<(), Error> {
 
     let resized = imageops::resize(&raw_img, new_w, new_h, FilterType::Triangle);
 
-    let pad_x = (target - new_w) / 2 as u32;
-    let pad_y = (target - new_h) / 2 as u32;
+    let pad_x = ((target - new_w) / 2) as i64;
+    let pad_y = ((target - new_h) / 2) as i64;
 
     println!("Padding: x={} y={}", pad_x, pad_y);
 
     // save resized image for debugging
-    let mut letterboxed = DynamicImage::new_rgb8(target, target);
-    imageops::replace(&mut letterboxed, &resized, pad_x as i64, pad_y as i64);
-    letterboxed.save("images/letterboxed.jpg")?;
+    let mut padded = DynamicImage::new_rgb8(target, target);
+    imageops::replace(&mut padded, &resized, pad_x, pad_y);
+    padded.save("images/padded.jpg")?;
+
+    // create input tensor in NCHW format https://machinelearning.wtf/terms/nchw/, normalized to 0..1
+    let mut input_arr = Array4::<f32>::zeros((1usize, 3usize, target as usize, target as usize));
+
+    for y in 0..(target as usize) {
+        for x in 0..(target as usize) {
+            let px = padded.get_pixel(x as u32, y as u32);
+            input_arr[[0, 0, y, x]] = px[0] as f32 / 255.0;
+            input_arr[[0, 1, y, x]] = px[1] as f32 / 255.0;
+            input_arr[[0, 2, y, x]] = px[2] as f32 / 255.0;
+        }
+    }
+
+    let input_tensor: Tensor = input_arr.into();
+
+    println!("Inference started at ... {:?}", t.elapsed());
+    // run inference
+    let outputs = model.run(tvec!(input_tensor.into()))?;
+    println!("Inference done in {:?}", t.elapsed());
+
+    println!("Output tensor shape: {:?}", outputs[0].shape());
 
     Ok(())
 }
